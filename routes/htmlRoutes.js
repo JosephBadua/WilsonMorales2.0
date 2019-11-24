1aa9baf89415985da2a5906ae6651c3fba594796
JosephBadua <josephbadua1006@gmail.com>
2019-11-24 01:09:41 -0800

    added routes, and images, and about  me section

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,8 +3,32 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.render("index");
     });
-    app.get("/blog", function(req, res) {
-        res.render("blog");
+    app.get("/colposcopy", function(req, res) {
+        res.render("colposcopy");
+    });
+    app.get("/c-section", function(req, res) {
+        res.render("c-section");
+    });
+    app.get("/cervix", function(req, res) {
+        res.render("cervix");
+    });
+    app.get("/curettage", function(req, res) {
+        res.render("curettage");
+    });
+    app.get("/gynecological", function(req, res) {
+        res.render("gynecological");
+    });
+    app.get("/gynecoloic", function(req, res) {
+        res.render("gynecoloic");
+    });
+    app.get("/myomectomy", function(req, res) {
+        res.render("myomectomy");
+    });
+    app.get("/hysterectomy", function(req, res) {
+        res.render("hysterectomy");
+    });
+    app.get("/laproscopy", function(req, res) {
+        res.render("laproscopy");
     });
 };
   
