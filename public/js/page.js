1ac2b5e7708f02aff2419eb0ba63308cd153b3a6
JosephBadua <josephbadua1006@gmail.com>
2019-11-25 11:40:10 -0800

    carousel sizing fix

--- public/js/page.js
+++ public/js/page.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({full_width: true});
      setInterval(function() {
        $('.carousel').carousel('next');
      }, 4500);   
