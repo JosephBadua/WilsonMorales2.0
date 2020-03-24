ac3be33a2079f09bcce9b256153054689e0436ab
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-03-24 05:01:49 -0700

    reinit files

--- public/js/page.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.carousel').carousel();
-    $('.carousel.carousel-slider').carousel({full_width: true});
-     setInterval(function() {
-       $('.carousel').carousel('next');
-     }, 4500);   
-});
